1ba0a8a2a3a35fe9b6c4829fb61e0749c347dd70
Yatsu0720 <witdesigns@yatsu-world.com>
2022-06-29 19:06:31 +0900

    memo.app ver1.0

---
+++ memo_app.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'webrick'
+require 'net/http'
+
+get '/' do
+  @memo_title = params[:memo_title]
+  @memo_text = params[:memo_text]
+  @memo_id = params[:id]
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  @json_data = json_data
+
+  erb :index, layout: :layout
+end
+
+get '/new' do
+  erb :new_memo
+end
+
+get '/memo' do
+  erb :memo
+end
+
+get '/memo/:name' do
+  @memo_id = params[:name]
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  @json_data = json_data
+  @memo_title = @json_data.fetch(@memo_id).keys[0]
+  @memo_text = @json_data.fetch(@memo_id).fetch(@memo_title)
+
+  erb :memo
+end
+
+get '/memo/:name/edit' do
+  @memo_id = params[:name]
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  @json_data = json_data
+  @memo_title = @json_data.fetch(@memo_id).keys[0]
+  @memo_text = @json_data.fetch(@memo_id).fetch(@memo_title)
+
+  erb :edit
+end
+
+post '/' do
+  @memo_title = h(params[:memo_title]).to_s
+  @memo_text = h(params[:memo_text]).to_s
+  @memo_id = h(params[:id]).to_s
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  value = { @memo_title => @memo_text }
+  json_data[@memo_id] = value
+
+  @json_data = json_data
+
+  open('memo_data.json', 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+
+  erb :index
+end
+
+patch '/memo/:name/edit' do
+  @memo_id = params[:name]
+
+  @memo_title = h(params[:memo_title]).to_s
+  @memo_text = h(params[:memo_text]).to_s
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  json_data[@memo_id] = { @memo_title => @memo_text }
+
+  @json_data = json_data
+
+  open('memo_data.json', 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+
+  redirect "/memo/#{@memo_id}"
+
+  erb :edit
+end
+
+delete '/memo/:name' do
+  @memo_id = params[:name]
+
+  json_data = open('memo_data.json') do |file|
+    JSON.load(file)
+  end
+
+  json_data.delete(@memo_id)
+
+  @json_data = json_data
+
+  open('memo_data.json', 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+
+  redirect '/'
+
+  erb :index
+end
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+error do
+  erb :notfound
+end
