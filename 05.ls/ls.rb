314290f5b870e2d777a5a9467502350c65e95224
Yatsu0720 <witdesigns@yatsu-world.com>
2022-07-16 22:17:58 +0900

    データ格納方法を配列からハッシュに変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -25,11 +25,13 @@ MAX_COLUMN = 3
 end
 
 def fetch_file_list
-  number_of_hard_link = []   
-  user_name = []
-  group_name = []
-  file_size = []
-  timestamp = [] 
+  ll_display = []
+  file_state_chars = []
+  hard_links = []   
+  user_names = []
+  group_names = []
+  file_sizes = []
+  timestamps = [] 
 
   options = ARGV.getopts('l')
   ls_dir = ARGV[0] || '.'
@@ -49,23 +51,28 @@ def fetch_file_list
         
     correspondence_table = {'0'=> '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}
 
-    number_of_hard_link[integer] = file_state.nlink
-    user_name[integer] = Etc.getpwuid(file_state.uid).name
-    group_name[integer] = Etc.getgrgid(file_state.gid).name
-    file_size[integer] = file_state.size
-    timestamp[integer] = file_state.mtime.strftime('%b %d %R')
-
+    file_state_chars = file_type + correspondence_table[file_state_array[3]] + correspondence_table[file_state_array[4]] + correspondence_table[file_state_array[5]]
+    
+    ll_display << {file_state: file_state_chars, hard_link: file_state.nlink.to_s, user_name: Etc.getpwuid(file_state.uid).name, group_name: Etc.getgrgid(file_state.gid).name, file_size: file_state.size.to_s, timestamps: file_state.mtime.strftime('%b %d %R')}
   end
+ 
+ # max_hard_link = hard_links.max_by { |hard_link| hard_link.length }.length
+ # max_user_name = user_names.max_by { |user_name| user_name.length }.length 
+ # max_group_name = group_names.max_by { |group_name| group_name.length }.length 
+ # max_file_size = file_sizes.max_by { |file_size| file_size.length }.length
+ # max_timestamps = timestamps.max_by {|timestamp| timestamp.length }.length
+  
+ # hard_links_array = hard_links.map {|hard_link| hard_link.rjust(max_hard_link, ' ')}
+ # user_names_array = user_names.map{|user_name| user_name.rjust(max_user_name, ' ')}
+ # group_names_array = group_names.map{|group_name| group_name.rjust(max_group_name, ' ')}
+ # file_sizes_array = file_sizes.map{ |file_size| file_size.rjust(max_file_size, ' ')}
 
   binding.irb
 
-  if options['r']
-    file_list.sort do |a,b|
+  # if options['r']
       
-    end.reverse
-  else
-    file_list.sort
-  end
 end
 
-print_file_list
+fetch_file_list
+
+# print_file_list
