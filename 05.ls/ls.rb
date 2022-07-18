93c4634fb7233e9ff841af908c1dbc9ea666c109
Yatsu0720 <witdesigns@yatsu-world.com>
2022-07-19 07:40:49 +0900

    lコマンド（引数で指定は未）一旦完成

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -7,7 +7,7 @@ require 'etc'
 MAX_COLUMN = 3
 
  
-      def print_file_list
+def print_file_list
   file_list = fetch_file_list
 
   max_word_count = file_list.max_by(&:length).length
@@ -35,11 +35,15 @@ def fetch_file_list
   options = ARGV.getopts('l')
   ls_dir = ARGV[0] || '.'
 
-  file_list = Dir.foreach(ls_dir).to_a.reject { |file_name| file_name.start_with?('.') }
+  file_list = Dir.foreach(ls_dir).to_a.reject { |file_name| file_name.start_with?('.') }.sort
+  number_of_files = file_list.size
+
+
   file_list.each_with_index do |file, integer|
-    file_state = File.stat(file)
+    file_state = File.stat(ls_dir + '/' + file)
+
     file_state_array = file_state.mode.to_s(8).rjust(6, '0').split('')
-    
+
     file_type = if file_state_array[0] + file_state_array[1] == '10'
                   '-'
                 elsif file_state_array[0] + file_state_array[1] == '04'
@@ -52,24 +56,40 @@ def fetch_file_list
 
     file_state_chars = file_type + correspondence_table[file_state_array[3]] + correspondence_table[file_state_array[4]] + correspondence_table[file_state_array[5]]
     
-    long_format_files << {file_state: file_state_chars, hard_link: file_state.nlink.to_s, user_name: Etc.getpwuid(file_state.uid).name, group_name: Etc.getgrgid(file_state.gid).name, file_size: file_state.size.to_s, timestamps: file_state.mtime.strftime('%b %d %R'), file_name: file_list}
+    long_format_files << {file_blocks_size: file_state.blocks, file_state: file_state_chars, hard_link: file_state.nlink.to_s, user_name: Etc.getpwuid(file_state.uid).name, group_name: Etc.getgrgid(file_state.gid).name, file_size: file_state.size.to_s, timestamps: file_state.mtime.strftime('%b %d %R'), file_name: file_list[integer]}
   end
   max_hard_link = long_format_files.map { |x| x[:hard_link] }.max_by { |hard_link| hard_link.length }.length
   max_user_name = long_format_files.map { |x| x[:user_name] }.max_by { |user_name| user_name.length }.length
   max_group_name = long_format_files.map { |x| x[:group_name] }.max_by { |group_name| group_name.length }.length
   max_file_size = long_format_files.map { |x| x[:file_size] }.max_by { |file_size| file_size.length }.length
-  j
+  max_file_name = long_format_files.map { |long_format_file| long_format_file[:file_name] }.max_by { |file_name| file_name.length }.length
+ 
+  file_states_array = long_format_files.map{ |long_format_file| long_format_file[:file_state] }
+     
+  hard_links_array = long_format_files.map { |long_format_file| long_format_file[:hard_link]}
+  rjust_hard_links_array = hard_links_array.map {|hard_link| hard_link.rjust(max_hard_link, ' ') }
+ 
+  user_names_array = long_format_files.map { |long_format_file| long_format_file[:user_name]}
+  rjust_user_names_array = user_names_array.map {|user_name| user_name.rjust(max_user_name, ' ')}
+
+  group_names_array = long_format_files.map{ |long_format_file| long_format_file[:group_name]}
+  rjust_group_names_array = group_names_array.map {|group_name| group_name.rjust(max_group_name + 1, ' ')}
+
+  file_sizes_array = long_format_files.map{ |long_format_file| long_format_file[:file_size] } 
+  rjust_file_sizes_array = file_sizes_array.map{ |file_size| file_size.rjust(max_file_size + 1, ' ')}
+
+  timestamps_array = long_format_files.map{ |long_format_file| long_format_file[:timestamps] }
+
+  file_names_array = long_format_files.map{ |long_format_file| long_format_file[:file_name] }
+  ljust_file_names_array = file_names_array.map{ |file_name| file_name.ljust(max_file_name, ' ')}
   
- # hard_links_array = hard_links.map {|hard_link| hard_link.rjust(max_hard_link, ' ')}
- # user_names_array = user_names.map{|user_name| user_name.rjust(max_user_name, ' ')}
- # group_names_array = group_names.map{|group_name| group_name.rjust(max_group_name, ' ')}
- # file_sizes_array = file_sizes.map{ |file_size| file_size.rjust(max_file_size, ' ')}
+  long_format_files_array = [file_states_array, rjust_hard_links_array, rjust_user_names_array, rjust_group_names_array, rjust_file_sizes_array, timestamps_array, ljust_file_names_array].transpose 
 
+  puts ('total ' + long_format_files.sum{ |i| i[:file_blocks_size] }.to_s)
 
+  long_format_files_array.each { |long_format_file| puts(long_format_file.join(' '))}
   # if options['r']
-      
 end
-
 fetch_file_list
 
 # print_file_list
