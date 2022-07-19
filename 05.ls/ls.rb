2b714aaa405da47b307cdebcdee10423980a6d1c
Yatsu0720 <witdesigns@yatsu-world.com>
2022-07-19 21:35:44 +0900

    オプションなしの場合にも対応

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,26 +5,33 @@ require 'optparse'
 require 'etc'
 
 MAX_COLUMN = 3
+CORRESPONDENCE_TABLE = {'0'=> '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}
 
- 
 def print_file_list
-  file_list = fetch_file_list
+  if fetch_command[0]['l']
+    fetch_file
+  else
+    output_normal
+  end
+end
 
-  max_word_count = file_list.max_by(&:length).length
-  max_row = file_list.size / MAX_COLUMN + 1
+def output_normal
+  max_word_count = fetch_file_list.max_by(&:length).length
+  max_row = fetch_file_list.size / MAX_COLUMN + 1
 
-  separated_list = file_list.each_slice(max_row).to_a
+  separated_list = fetch_file_list.each_slice(max_row).to_a
 
   formatted_list = separated_list.map do |file_names|
     file_names.values_at(0..max_row - 1).map do |file_name|
-      file_name.to_s.ljust(5 + max_word_count)
-    end
+    file_name.to_s.ljust(5 + max_word_count) 
   end
-
-  puts formatted_list.transpose.map(&:join)
 end
 
-def fetch_file_list
+   puts formatted_list.transpose.map(&:join)
+
+end
+
+def fetch_file
   long_format_files = []
   file_state_chars = []
   hard_links = []   
@@ -32,18 +39,12 @@ def fetch_file_list
   group_names = []
   file_sizes = []
 
-  options = ARGV.getopts('l')
-  ls_dir = ARGV[0] || '.'
+  number_of_files = fetch_file_list.size
 
-  file_list = Dir.foreach(ls_dir).to_a.reject { |file_name| file_name.start_with?('.') }.sort
-  number_of_files = file_list.size
-
-
-  file_list.each_with_index do |file, integer|
-    file_state = File.stat(ls_dir + '/' + file)
-
+  fetch_file_list.each_with_index do |file, integer|
+    file_state = File.stat( fetch_command[1] + '/' + file)
+    
     file_state_array = file_state.mode.to_s(8).rjust(6, '0').split('')
-
     file_type = if file_state_array[0] + file_state_array[1] == '10'
                   '-'
                 elsif file_state_array[0] + file_state_array[1] == '04'
@@ -52,12 +53,16 @@ def fetch_file_list
                   'l'
                 end
         
-    correspondence_table = {'0'=> '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}
 
-    file_state_chars = file_type + correspondence_table[file_state_array[3]] + correspondence_table[file_state_array[4]] + correspondence_table[file_state_array[5]]
+    file_state_chars = file_type + CORRESPONDENCE_TABLE[file_state_array[3]] + CORRESPONDENCE_TABLE[file_state_array[4]] + CORRESPONDENCE_TABLE[file_state_array[5]]
     
-    long_format_files << {file_blocks_size: file_state.blocks, file_state: file_state_chars, hard_link: file_state.nlink.to_s, user_name: Etc.getpwuid(file_state.uid).name, group_name: Etc.getgrgid(file_state.gid).name, file_size: file_state.size.to_s, timestamps: file_state.mtime.strftime('%b %d %R'), file_name: file_list[integer]}
+    max_hard_lin = [] << file_state.nlink.to_s
+
+
+
+    long_format_files << {file_blocks_size: file_state.blocks, file_state: file_state_chars, hard_link: file_state.nlink.to_s, user_name: Etc.getpwuid(file_state.uid).name, group_name: Etc.getgrgid(file_state.gid).name, file_size: file_state.size.to_s, timestamps: file_state.mtime.strftime('%b %d %R'), file_name: fetch_file_list[integer]}
   end
+ 
   max_hard_link = long_format_files.map { |x| x[:hard_link] }.max_by { |hard_link| hard_link.length }.length
   max_user_name = long_format_files.map { |x| x[:user_name] }.max_by { |user_name| user_name.length }.length
   max_group_name = long_format_files.map { |x| x[:group_name] }.max_by { |group_name| group_name.length }.length
@@ -88,8 +93,17 @@ def fetch_file_list
   puts ('total ' + long_format_files.sum{ |i| i[:file_blocks_size] }.to_s)
 
   long_format_files_array.each { |long_format_file| puts(long_format_file.join(' '))}
-  # if options['r']
 end
-fetch_file_list
 
-# print_file_list
+def fetch_file_list
+  file_list = Dir.foreach(fetch_command[1]).to_a.reject { |file_name| file_name.start_with?('.') }.sort
+end
+
+def fetch_command
+  options = ARGV.getopts('l')
+  ls_dir = ARGV[0] || '.'
+
+  return options, ls_dir
+end
+
+print_file_list
