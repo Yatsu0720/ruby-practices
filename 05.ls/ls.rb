ca8ba54369892fcc2466df6cf13c01b9517b3097
Yatsu0720 <witdesigns@yatsu-world.com>
2022-08-14 08:22:08 +0900

    追加の指摘事項を修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,6 +3,7 @@
 
 require 'optparse'
 require 'etc'
+require 'debug'
 
 MAX_COLUMN = 3
 PERMISSION_CONVERSION = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
@@ -14,8 +15,9 @@ def main
   opts = OptionParser.new
   opts.on('-l') { l_option = true }
   opts.parse!(ARGV) || '.'
+  ls_dir ||= ARGV[0] || '.'
 
-  file_stats = make_file_stats
+  file_stats = make_file_stats(ls_dir)
   if l_option
     print_l_option_files(file_stats)
   else
@@ -44,11 +46,11 @@ def print_l_option_files(file_stats)
   puts "total #{total_block_size}"
   file_stats.each do |file_stat|
     print file_stat[:permission]
-    print "#{file_stat[:hard_link].rjust(max_lengths[:hard_link] + 1)} "
+    print "#{file_stat[:hard_link].to_s.rjust(max_lengths[:hard_link] + 1)} "
     print file_stat[:user].ljust(max_lengths[:user] + 2)
     print file_stat[:group].ljust(max_lengths[:group] + 2)
-    print file_stat[:file_size].rjust(max_lengths[:file_size])
-    print file_stat[:time]
+    print file_stat[:file_size].to_s.rjust(max_lengths[:file_size])
+    print file_stat[:time].strftime(' %b %e %R ')
     puts file_stat[:file_name]
   end
 end
@@ -56,10 +58,10 @@ end
 def calculate_max_length(files_stats)
   max_lengths = {}
   files_stats.each do |hash|
-    max_lengths[:hard_link] = [max_lengths[:hard_link].to_i, hash[:hard_link].to_i].max.to_s.size
-    max_lengths[:user] = [max_lengths[:user].to_s, hash[:user]].max.size
-    max_lengths[:group] = [max_lengths[:group].to_s, hash[:group]].max.size
-    max_lengths[:file_size] = [max_lengths[:file_size].to_i, hash[:file_size].to_i].max.to_s.size
+    max_lengths[:hard_link] = [max_lengths[:hard_link].to_i, hash[:hard_link].to_s.size].max
+    max_lengths[:user] = [max_lengths[:user].to_i, hash[:user].size].max
+    max_lengths[:group] = [max_lengths[:group].to_i, hash[:group].size].max
+    max_lengths[:file_size] = [max_lengths[:file_size].to_i, hash[:file_size].to_s.size].max
   end
   max_lengths
 end
@@ -73,30 +75,28 @@ def convert_file_stat_chars(file_stat)
   file_type + owner_permission + group_permission + other_permission
 end
 
-def make_file_stats
-  files, ls_dir = fetch_files
+def make_file_stats(ls_dir)
+  files = fetch_files(ls_dir)
   files.map do |file|
-    Dir.chdir(ls_dir) do
-      file_stat = File.stat(file)
-      file_stats = {}
-      file_stats[:blocks_size] = file_stat.blocks
-      file_stats[:permission] = convert_file_stat_chars(file_stat)
-      file_stats[:hard_link] = file_stat.nlink.to_s
-      file_stats[:user] = Etc.getpwuid(file_stat.uid).name
-      file_stats[:group] = Etc.getgrgid(file_stat.gid).name
-      file_stats[:file_size] = file_stat.size.to_s
-      file_stats[:time] = file_stat.mtime.strftime(' %b %e %R ')
-      file_stats[:file_name] = File.basename(file)
-      file_stats
-    end
+    file_stat = File.stat(file)
+    file_stats = {}
+    file_stats[:blocks_size] = file_stat.blocks
+    file_stats[:permission] = convert_file_stat_chars(file_stat)
+    file_stats[:hard_link] = file_stat.nlink
+    file_stats[:user] = Etc.getpwuid(file_stat.uid).name
+    file_stats[:group] = Etc.getgrgid(file_stat.gid).name
+    file_stats[:file_size] = file_stat.size
+    file_stats[:time] = file_stat.mtime
+    file_stats[:file_name] = File.basename(file)
+    file_stats
   end
 end
 
-def fetch_files
-  ls_dir ||= ARGV[0] || '.'
+def fetch_files(ls_dir)
   Dir.chdir(ls_dir) do
-    files = Dir.glob('*').sort
-    return files, ls_dir
+    files = Dir.glob("*").sort.map do |file|
+      "#{ls_dir}/#{file}"
+    end
   end
 end
 
