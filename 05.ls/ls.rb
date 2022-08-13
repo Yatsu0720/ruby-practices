7043beadc00fc313ec53e695aae8d526e5131962
Yatsu0720 <witdesigns@yatsu-world.com>
2022-08-13 14:25:01 +0900

    不要なライブラリを削除

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,7 +3,6 @@
 
 require 'optparse'
 require 'etc'
-require 'debug'
 
 MAX_COLUMN = 3
 PERMISSION_CONVERSION = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
